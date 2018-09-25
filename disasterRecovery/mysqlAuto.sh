fd74871916d4ab9077dc6293955b84badfc4efed
Makhate Makhate <makhatemakhate@gmail.com>
2018-09-25 11:22:48 +0200

    some small ground work on setting variables

--- disasterRecovery/mysqlAuto.sh
+++ disasterRecovery/mysqlAuto.sh
@@ -1,1 +1,13 @@
+#!/bin/bash
+
+#Setting constant variables. These ones will rarely be changed
+$containerDir = ""
+$host_folder = ""
+
+#Setting flexible variables. These will be changed according to facility name and
+#Google ID folder for that facility
+$facilityname = "sdahc"
+$folderid = ""
+
+
 
