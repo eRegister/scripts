492635905aa02866f187766b1315bffac083bd11
Makhate Makhate <makhatemakhate@gmail.com>
2018-09-27 14:51:32 +0200

    very close to OK

--- disasterRecovery/mysqlAuto.sh
+++ disasterRecovery/mysqlAuto.sh
@@ -2,16 +2,25 @@
 
 #Setting constant variables. These ones will rarely be changed
 #$containerDir = "/backups"
-$host_folder = "/home/makhate/backups"
+set today = 'date +%Y'
+set site_home = "/home/makhate"
+set uploadtoDrive = "/home/makhate/lib/gdrive-linux-x64"
+
+echo "directory for site_home"
+echo $site_home
+echo "directory for uploadtoDrive"
+echo $uploadtoDrive
 
 #Setting flexible variables. These will be changed according to facility name and
 #Google ID folder for that facility
-$facilityname = "sdahc"
-$folderid = "1NCqMO6rCehpWUyeHg9wgJMy0S8HALuFz"
+#$facilityname = "sdahc"
+set folderid = "1NCqMO6rCehpWUyeHg9wgJMy0S8HALuFz"
 
 #creating backup
-docker exec -it eregister /usr/bin/mysqldump -u root --password=password openmrs > /home/makhate/backups/backup.sql
+#docker exec -it eregister /usr/bin/mysqldump -u root --password=password openmrs > $site_home/backups/backup.sql
+docker exec -it eregister /usr/bin/mysqldump -u root --password=password openmrs > /home/makhate/backups/sebata.sql
 
-#copying backup out of containter
 
-gdrive-linux-x64 upload --parent $folderid omrs_latest_backup_12092018.sql
+#uploading to Google Drive
+#$uploadtoDrive upload --parent $folderid $site_home/backups/leribe.sql
+$uploadtoDrive upload --parent 1NCqMO6rCehpWUyeHg9wgJMy0S8HALuFz /home/makhate/backups/sebata.sql
