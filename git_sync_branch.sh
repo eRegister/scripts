f4d2b5334165c8481d9ba698f2e252953a02c006
Makhate Makhate <makhatemakhate@gmail.com>
2020-03-03 17:41:10 +0200

    using PULL instead

--- git_sync_branch.sh
+++ git_sync_branch.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-gitDir=/var/www/bahmni_config_release/.git
+gitDir=/development/bahmni_config_release/.git
 
 # checks if something happened on the remote repo
-git --git-dir=$gitDir fetch
+# git --git-dir=$gitDir fetch
 
 # now see if there's anything that happened
 git --git-dir=$gitDir diff HEAD remotes/origin/master
@@ -11,4 +11,4 @@ git --git-dir=$gitDir diff HEAD remotes/origin/master
 # Now get ONLY files that were edited. This will
 # help in preserving bandwith
 
-git --git-dir=$gitDir fetch origin
+git --git-dir=$gitDir pull origin
