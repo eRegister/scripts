b745e92a310a6d33ec1fd83bc874ef45c21aad00
Makhate Makhate <makhatemakhate@gmail.com>
2020-03-03 15:47:40 +0200

    Update dockerbootstrap.sh
    updating the script to the latest version

--- dockerbootstrap.sh
+++ dockerbootstrap.sh
@@ -1,46 +1,6 @@
 #!/bin/bash
 
-#this script will be run by a cronjob
-#put it in ~/lib (OR $HOME/lib)
-#then we'll tell the OS to bootstrap docker
-#and its services at startup or reboot
-#follow this steps:
-#put the script in $HOME/lib/
-#go to the lid directory [cd $HOME/lib]
-#make the script to be executable with this [chmod +x ./dockerbootstrap.sh]
-#run this command [crontab -e]
-#edit the cron editor like so:
-#@reboot $HOME/lib/dockerbootstrap.sh
-
-container=openmrseregister
-
-#$docker restart $container
-
-#docker exec -it $container service httpd start
-
-#docker exec -it $container service mysqld start
-
-#docker exec -it $container service openmrs start
-
-case $1 in 
-
-start)
-    docker start $container
-    docker exec -it $container service httpd start
-    docker exec -it $container service mysqld start
-    docker exec -it $container service openmrs start
-    ;;
-stop)
-    docker stop $container
-    ;;
-restart
-    docker stop $container
-    sleep 6
-    docker start $container
-    docker exec -it $container service httpd start
-    docker exec -it $container service mysqld start
-    docker exec -it $container service openmrs start
-    ;;
-esac
-
-exit 0
+docker exec -i openmrseregister service httpd start
+docker exec -i openmrseregister service openmrs start
+docker exec -i openmrseregister service bahmni-reports start
+docker exec -i openmrseregister sh /var/www/bahmni_config_release/git_sync_branch.sh
